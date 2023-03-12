36151ec8c25ee2885f86932ff4f61f1740f57c7e
Alex Baskakau <lenovos6000v4@gmail.com>
2023-03-12 23:50:48 +0000

    First commit

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -3,6 +3,7 @@ package ru.netology.stats;
 
 public class StatsService {
 
+
     public int minSales(long[] sales) {
         int minMonth = 0; // номер месяца с минимальными продажами среди просмотренных ранее
 
@@ -19,8 +20,7 @@ public class StatsService {
         long sum = 0;
 
         for (int i = 0; i < sales.length; i++) {
-            // if (sales[i] <= sales[minMonth]) { // значит, в рассматриваемом i-м месяце продаж меньше
-            // minMonth = i; // запомним его как минимальный
+
             sum = sum + sales[i];
         }
         return sum;
@@ -32,17 +32,17 @@ public class StatsService {
             sum = sum + sales[i];
         }
         long average = sum / sales.length;
+
         return average;
 
     }
 
-
     public int maxSales(long[] sales) {
-        int maxMonth = 0; // номер месяца с минимальными продажами среди просмотренных ранее
+        int maxMonth = 0;
 
         for (int i = 0; i < sales.length; i++) {
-            if (sales[i] >= sales[maxMonth]) { // значит, в рассматриваемом i-м месяце продаж меньше
-                maxMonth = i; // запомним его как минимальный
+            if (sales[i] >= sales[maxMonth]) {
+                maxMonth = i;
             }
         }
 
